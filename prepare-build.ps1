016c7496d245de2543e0bea3c504905f73ae80fb
Samriddha Chatterjee <Samriddha.Chatterjee@hcl.com>
2018-11-15 15:27:56 +0530

    powershell2

--- prepare-build.ps1
+++ prepare-build.ps1
@@ -1,1 +1,22 @@
-Get-Command
+[CmdletBinding()]
+if(-not ($Env:BUILD_SOURCESDIRECTORY -and $Env:BUILD_BUILDNUMBER))
+{
+    Write-Error "You must set the following environment variables"
+    Write-Error "to test this script interactively."
+    Write-Host '$Env:BUILD_SOURCESDIRECTORY - For example, enter something like:'
+    Write-Host '$Env:BUILD_SOURCESDIRECTORY = "C:\code\FabrikamTFVC\HelloWorld"'
+    Write-Host '$Env:BUILD_BUILDNUMBER - For example, enter something like:'
+    Write-Host '$Env:BUILD_BUILDNUMBER = "Build HelloWorld_0000.00.00.0"'
+    exit 1
+}
+if (-not $Env:BUILD_SOURCESDIRECTORY)
+{
+    Write-Error ("BUILD_SOURCESDIRECTORY environment variable is missing.")
+    exit 1
+}
+elseif (-not (Test-Path $Env:BUILD_SOURCESDIRECTORY))
+{
+    Write-Error "BUILD_SOURCESDIRECTORY does not exist: $Env:BUILD_SOURCESDIRECTORY"
+    exit 1
+}
+Write-Verbose "BUILD_SOURCESDIRECTORY: $Env:BUILD_SOURCESDIRECTORY"
